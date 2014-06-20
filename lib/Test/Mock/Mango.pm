f04acca7e4b4867bd06ca0d417eea9f479c82e3b
John Gregory <john@johngregory.me.uk>
2014-06-20 14:58:07 +0100

    Up version

--- lib/Test/Mock/Mango.pm
+++ lib/Test/Mock/Mango.pm
@@ -4,7 +4,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.07';
 
 require 'Mango.pm'; # Bit useless if you don't actually have mango
 use Test::Mock::Mango::FakeData;
